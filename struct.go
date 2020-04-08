8aad29a9e1a5555908b49558fe200a8720400439
Shenli Hartono <shenlihartono@gmail.com>
2020-04-08 19:41:41 +0700

    add domain types and service contract

---
+++ struct.go
@@ -0,0 +1,19 @@
+// Package groot is the root package containing the domain types and service contracts associated with the types.
+package groot
+
+type Struct struct {
+	ID    string `json:"id"`
+	Value int    `json:"value"`
+}
+
+type StructRepository interface {
+	Create(StructRequest) (Struct, error)
+	Struct(ID string) (Struct, error)
+	Structs() ([]Struct, error)
+	Update(ID string, req StructRequest) error
+	Delete(ID string)
+}
+
+type StructRequest struct {
+	Value int
+}
